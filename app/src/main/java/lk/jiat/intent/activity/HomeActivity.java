eb4c343eabf65301097c03e9bf15aedcad0d4479
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-15 12:30:11 +0530

    onCreate

--- app/src/main/java/lk/jiat/intent/activity/HomeActivity.java
+++ app/src/main/java/lk/jiat/intent/activity/HomeActivity.java
@@ -1,6 +1,15 @@
 package lk.jiat.intent.activity;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState);
+    }
 }
