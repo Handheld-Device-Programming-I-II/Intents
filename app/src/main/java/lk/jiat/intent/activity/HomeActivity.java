2b85e5230b37ebaea2bd345411e43e09814395e5
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-15 23:21:51 +0530

    R.layout

--- app/src/main/java/lk/jiat/intent/activity/HomeActivity.java
+++ app/src/main/java/lk/jiat/intent/activity/HomeActivity.java
@@ -6,12 +6,15 @@ import android.os.PersistableBundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import lk.jiat.intent.R;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState);
         //layout file configuration and initialization
+        setContentView(R.layout.activity_home);
     }
 
     @Override
