5a27512fdc2914d71eff1c19f267615279624da9
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-16 21:42:39 +0530

    create settingBtn object

--- app/src/main/java/lk/jiat/intent/activity/SettingActivity.java
+++ app/src/main/java/lk/jiat/intent/activity/SettingActivity.java
@@ -1,6 +1,7 @@
 package lk.jiat.intent.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +9,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import lk.jiat.intent.R;
 
 public class SettingActivity extends AppCompatActivity {
-
+    private Button settingBtn;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
     }
 }
