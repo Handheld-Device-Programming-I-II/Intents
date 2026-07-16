1f886a6f56836082d1df9cb65297dd3f9b9fcfef
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-16 21:43:55 +0530

    R.id.settingBtn

--- app/src/main/java/lk/jiat/intent/activity/SettingActivity.java
+++ app/src/main/java/lk/jiat/intent/activity/SettingActivity.java
@@ -14,6 +14,6 @@ public class SettingActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
+        this.settingBtn=findViewById(R.id.settingBtn);
     }
 }
