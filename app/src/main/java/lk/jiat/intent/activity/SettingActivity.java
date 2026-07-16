3e43d08cea04a9b015039e37d998a1a1349fa112
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-16 21:34:44 +0530

    R.layout

--- app/src/main/java/lk/jiat/intent/activity/SettingActivity.java
+++ app/src/main/java/lk/jiat/intent/activity/SettingActivity.java
@@ -1,7 +1,6 @@
 package lk.jiat.intent.activity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
