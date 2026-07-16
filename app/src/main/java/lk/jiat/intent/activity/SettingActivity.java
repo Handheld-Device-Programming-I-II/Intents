027bc49a63f01830573ef3445d19ab44cf2581b0
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-16 20:50:04 +0530

    onCreate

--- app/src/main/java/lk/jiat/intent/activity/SettingActivity.java
+++ app/src/main/java/lk/jiat/intent/activity/SettingActivity.java
@@ -1,6 +1,18 @@
 package lk.jiat.intent.activity;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import lk.jiat.intent.R;
+
 public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
 }
