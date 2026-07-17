6d9f1444a32980346ef80ea275596d995c7b1ef8
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-17 18:38:35 +0530

    intent object

--- app/src/main/java/lk/jiat/intent/activity/SettingActivity.java
+++ app/src/main/java/lk/jiat/intent/activity/SettingActivity.java
@@ -1,5 +1,6 @@
 package lk.jiat.intent.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,5 +28,10 @@ public class SettingActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void backToHome(){
+        Intent intent = new Intent(SettingActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
