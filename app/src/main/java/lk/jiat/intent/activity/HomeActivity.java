64708614fa2e378610ff5710aca011aed00fd2ec
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-17 19:00:32 +0530

    sold error1

--- app/src/main/java/lk/jiat/intent/activity/HomeActivity.java
+++ app/src/main/java/lk/jiat/intent/activity/HomeActivity.java
@@ -14,7 +14,7 @@ import lk.jiat.intent.R;
 public class HomeActivity extends AppCompatActivity {
     private Button homeBtn;
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //layout file configuration and initialization
         setContentView(R.layout.activity_home);
