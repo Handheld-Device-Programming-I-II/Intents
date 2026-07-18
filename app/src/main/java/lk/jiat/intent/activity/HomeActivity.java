5d7ea5e4c3c1047855379499e4ab7ccd7b61f3e8
kalharaweragoda <kalharaweragoda@gmail.com>
2026-07-18 12:34:08 +0530

    intent object

--- app/src/main/java/lk/jiat/intent/activity/HomeActivity.java
+++ app/src/main/java/lk/jiat/intent/activity/HomeActivity.java
@@ -3,6 +3,7 @@ package lk.jiat.intent.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,6 +21,10 @@ public class HomeActivity extends AppCompatActivity {
         //layout file configuration and initialization
         setContentView(R.layout.activity_home);
         this.homeBtn = findViewById(R.id.homeBtn);
+
+        //access data
+        String username = getIntent().getStringExtra("username");
+        Log.i(SettingActivity.)
     }
 
     @Override
